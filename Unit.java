a0a626fcb22be0c50c7b6a9eb409dba4b26496f7
ChristopherWennendy <christopherwennendy@gmail.com>
2014-01-14 13:47:08 -0500

    Update Unit.java

--- Unit.java
+++ Unit.java
@@ -1,2 +1,22 @@
-public class Unit{
-  
+public abstract class Unit{
+    int movespace;
+    int attack;
+    int HP;
+    int def;
+    int mimrange;
+    int maxrange;
+    int xcoord;
+    int ycoord;
+    String name;
+    
+    Unit(){
+      movespace = 1;attack = 1; HP = 10; def = 1; minrange = 1; maxrange = 1;
+      name = "Rookie";
+    }
+    public int [] move(){
+      int [] coordinate = new int [2];
+      coordinate[0] = xcoord; coordinate[1] = ycoord;
+      ;
+      return 
+    }
+    
