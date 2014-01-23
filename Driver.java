35dca513ea60fbc6d6ce5250e2bb8375151297e3
Alexander Hu <ylsdhu@hotmail.com>
2014-01-23 18:27:11 -0500

    Attack testing

--- Driver.java
+++ Driver.java
@@ -3,9 +3,14 @@ public class Driver{
     Grid Field = new Grid();
     System.out.println(Field);
     Unit Testing = new Unit();
+    Unit noob = new Unit();
     Field.Place(Testing, 0, 0);
+    Field.Place(noob,0,1);
     System.out.println(Field);
-    Field.movedown(Testing);
+    System.out.println(Field.Status(0,0));
+    System.out.println(Field.Status(0,1));
+    Field.attack(Testing,noob);
+    System.out.println(Field.Status(0,1));
     System.out.println(Field);
   }
 }  
