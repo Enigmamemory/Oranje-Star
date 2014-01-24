823311de7aa4710368de7eea7d558fc516b12e42
Alex <ylsdhu@hotmail.com>
2014-01-23 20:15:39 -0500

    added deploy testing

--- Driver.java
+++ Driver.java
@@ -2,6 +2,7 @@ public class Driver{
   public static void main(String [] Args){
     Grid Field = new Grid();
     System.out.println(Field);
+    /*
     Unit Testing = new Unit();
     Unit noob = new Unit();
     Field.Place(Testing, 0, 0);
@@ -12,5 +13,10 @@ public class Driver{
     Field.attack(Testing,noob);
     System.out.println(Field.Status(0,1));
     System.out.println(Field);
+    */
+    Unit backup = new Unit();
+    Field.deploy(backup,0,0);
+    System.out.println(Field);
+    
   }
 }  
