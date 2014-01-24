0cbf65cc33c7f21eae0524bfaf0c50fbb7d49b92
ChristopherWennendy <christopherwennendy@gmail.com>
2014-01-24 01:43:08 -0500

    Update Unit.java

--- Unit.java
+++ Unit.java
@@ -1,19 +1,19 @@
 public class Unit{
-    boolean moved;
-    int movespace;
-    int defaultmove;
-    int attack;
-    int HP;
-    int defense;
-    int minrange;
-    int maxrange;
-    int xcoord;
-    int ycoord;
-    int cost;
-    String goodagainst;
-    String tiretype;
-    String side;
-    String name;
+    protected boolean moved;
+    protected int movespace;
+    protected int defaultmove;
+    protected int attack;
+    protected int HP;
+    protected int defense;
+    protected int minrange;
+    protected int maxrange;
+    protected int xcoord;
+    protected int ycoord;
+    protected int cost;
+    protected String goodagainst;
+    protected String tiretype;
+    protected String side;
+    protected String name;
     
     Unit(){
 	movespace = 1;attack = 1; HP = 10; defense = 1; minrange = 1; maxrange = 1;
@@ -32,67 +32,67 @@ public class Unit{
     }
     public void changeInfantry(){
     	defaultmove = 3;attack = 2;HP = 10;defense = 0;minrange = 0;maxrange = 1;cost = 1500;tiretype = "Foot";
-    	name = "Inftry"; protected String goodagainst = "Foot";
+    	name = "Inftry"; goodagainst = "Foot";
     }
     public void changeMech(){
     	defaultmove = 2;attack = 3;HP = 10;defense = 1;minrange = 0;maxrange = 1;cost = 2500;tiretype = "Foot";
-    	name = "Mech  "; protected String goodagainst = "Foot";
+    	name = "Mech  "; goodagainst = "Foot";
     }
     public void changeBike(){
     	defaultmove = 5;attack = 2;HP = 10;defense = 1;minrange = 0;maxrange = 1;cost = 2500;tiretype = "Tire";
-    	name = "Bike  "; protected String goodagainst = "Foot";
+    	name = "Bike  "; goodagainst = "Foot";
     }
     public void changeRecon(){
     	defaultmove = 8;attack = 2;HP = 10;defense = 1;minrange = 0;maxrange = 1;cost = 4000;tiretype = "Tire";
-    	name = "Recon "; protected String goodagainst = "None";
+    	name = "Recon "; goodagainst = "None";
     }
     public void changeAntiAirTank(){
     	defaultmove = 6;attack = 1;HP = 10;defense = 1;minrange = 0;maxrange = 1;cost = 7000;tiretype = "Tank";
-    	name = "AtArTk"; protected String goodagainst = "Air";
+    	name = "AtArTk"; goodagainst = "Air";
     }
     public void changeLightTank(){
     	defaultmove = 6;attack = 4;HP = 10;defense = 2;minrange = 0;maxrange = 1;cost = 7000;tiretype = "Tank";
-    	name = "LghtTk"; protected String goodagainst = "Foot";
+    	name = "LghtTk"; goodagainst = "Foot";
     }
     public void changeMediumTank(){
     	defaultmove = 5;attack = 5;HP = 10;defense = 3;minrange = 0;maxrange = 1;cost = 12000;tiretype = "Tank";
-    	name = "MedTnk"; protected String goodagainst = "Foot";
+    	name = "MedTnk"; goodagainst = "Foot";
     } 
     public void changeWarTank(){
     	defaultmove = 4;attack = 6;HP = 10;defense = 4;minrange = 0;maxrange = 1;cost = 16000;tiretype = "Tank";
-    	name = "WarTnk"; protected String goodagainst = "Foot";
+    	name = "WarTnk"; goodagainst = "Foot";
     }
     public void changeArtillery(){
     	defaultmove = 5;attack = 5;HP = 10;defense = 2;minrange = 2;maxrange = 3;cost = 6000;tiretype = "Tank";
-    	name = "Artlry"; protected String goodagainst = "Foot";
+    	name = "Artlry"; goodagainst = "Foot";
     }    
     public void changeAntiTank(){
     	defaultmove = 4;attack = 5;HP = 10;defense = 2;minrange = 1;maxrange = 3;cost = 11000;tiretype = "Tank";
-    	name = "AntiTk"; protected String goodagainst = "Tank";
+    	name = "AntiTk"; goodagainst = "Tank";
     }
     public void changeRocketLauncher(){
     	defaultmove = 5;attack = 4;HP = 10;defense = 1;minrange = 3;maxrange = 5;cost = 15000;tiretype = "Tank";
-    	name = "RktLch"; protected String goodagainst = "Foot";
+    	name = "RktLch"; goodagainst = "Foot";
     }
     public void AntiAirMissileLauncher(){
     	defaultmove = 5;attack = 1;HP = 10;defense = 2;minrange = 3;maxrange = 6;cost = 12000;tiretype = "Tank";
-    	name = "ArArML"; protected String goodagainst = "Air";
+    	name = "ArArML"; goodagainst = "Air";
     }
     public void changeFighter(){
     	defaultmove = 9;attack = 1;HP = 10;defense = 1;minrange = 0;maxrange = 1;cost = 20000;tiretype = "Air";
-    	name = "Fighter"; protected String goodagainst = "Air";
+    	name = "Fighter"; goodagainst = "Air";
     }
     public void changeBomber(){
     	defaultmove = 7;attack = 5;HP = 10;defense = 3;minrange = 0;maxrange = 1;cost = 20000;tiretype = "Air";
-    	name = "Bomber"; protected String goodagainst = "Tank";
+    	name = "Bomber"; goodagainst = "Tank";
     }
     public void changeDuster(){
     	defaultmove = 8;attack = 6;HP = 10;defense = 2;minrange = 0;maxrange = 1;cost = 13000;tiretype = "Air";
-    	name = "Duster"; protected String goodagainst = "Foot";
+    	name = "Duster"; goodagainst = "Foot";
     }
     public void changeBattleHelicopter(){
     	defaultmove = 6;attack = 2;HP = 10;defense = 2;minrange = 0;maxrange = 1;cost = 9000;tiretype = "Air";
-    	name = "BtlHcp"; protected String goodagainst = "Air";
+    	name = "BtlHcp"; goodagainst = "Air";
     }
     public String getUnit(){
 	return name;
