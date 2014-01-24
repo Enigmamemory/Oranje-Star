6bd38466c53e54210cb17dc7b03b6be5491734a4
Alex <ylsdhu@hotmail.com>
2014-01-23 20:04:18 -0500

    added candeploy and stuffs

--- Terrain.java
+++ Terrain.java
@@ -1,15 +1,26 @@
 public class Terrain {
-    private int capturerate;	
-    private String name;
+    protected boolean candeploy;
+    protected int capturerate;	
+    protected String name;
     
     Terrain(){
     	name = "Plain ";
+	capturerate = 20;
+	candeploy = false;
     }
 
     public String getname(){
 	return name;
     }
-
+    public void setname(String x){
+	name = x;
+    }
+    public boolean getdeploy(){
+	return candeploy;
+    }
+    public void setdeploy(boolean x){
+	candeploy = x;
+    }
     public String toString(){
 	return getname();
     }
@@ -19,4 +30,5 @@ public class Terrain {
     public void setcapturerate(int x){
     	capturerate = x;
     }
+    
 }
