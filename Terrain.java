466777238e49d7317c69955e809f10e36a82233c
ChristopherWennendy <christopherwennendy@gmail.com>
2014-01-24 01:44:42 -0500

    Update Terrain.java

--- Terrain.java
+++ Terrain.java
@@ -1,14 +1,14 @@
 public class Terrain {
-    boolean candeploy;
-    int capturerate;	
-    int defenseinc;
-    String name;
-    String side;
-    int movetakefoot;
-    int movetaketank;
-    int movetakeair;
-    int movetaketire;
-    int money;
+    protected boolean candeploy;
+    protected int capturerate;	
+    protected int defenseinc;
+    protected String name;
+    protected String side;
+    protected int movetakefoot;
+    protected int movetaketank;
+    protected int movetakeair;
+    protected int movetaketire;
+    protected int money;
     
     Terrain(){
     	name = "Plain ";
@@ -54,19 +54,19 @@ public class Terrain {
     }
     public void changeMountain(String x){
     	name = "Mntain"; candeploy = false; side = x;defenseinc = 4;
-    	movetakefoot = 2;movetaketank = 99;movetakeair = 1;movetaketire = 99; money = 1;
+    	movetakefoot = 2;movetaketank = 99;movetakeair = 1;movetaketire = 99; money = 0;
     }
     public void changeRoad(String x){
     	name = "Road  "; candeploy = false; side = x;defenseinc = 0;
-    	movetakefoot = 1;movetaketank = 1;movetakeair = 1;movetaketire = 1; money = 1;
+    	movetakefoot = 1;movetaketank = 1;movetakeair = 1;movetaketire = 1; money = 0;
     }    
     public void changeWasteland(String x){
     	name = "WstLnd"; candeploy = false; side = x;defenseinc = 2;
-    	movetakefoot = 1;movetaketank = 2;movetakeair = 1;movetaketire = 3; money = 1;
+    	movetakefoot = 1;movetaketank = 2;movetakeair = 1;movetaketire = 3; money = 0;
     }
     public void changeWood(String x){
     	name = "Wood  "; candeploy = false; side = x;defenseinc = 3;
-    	movetakefoot = 1;movetaketank = 2;movetakeair = 1;movetaketire = 3; money = 1;
+    	movetakefoot = 1;movetaketank = 2;movetakeair = 1;movetaketire = 3; money = 0;
     }    
     public String getname(){
 	return name;
